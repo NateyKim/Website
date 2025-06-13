b1baf8b469f4fe8d8a17e96cbd28d064701f7a2f
Nathaniel Kim <natey0214@gmail.com>
2025-06-13 16:00:01 -0400

    Mookie

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,14 @@ cd "C:\Users\Nathaniel Kim\my-website"
 
 <!-- 
  git add .
-git commit -m "Your descriptive message here"
+git commit -m "Testing name variable"
 git push
 -->
 <!-- 
 npx vercel deploy --prod
+project name = website
 -->
+
 
 <script>
   let name = 'Nathaniel Kim';
@@ -19,4 +21,7 @@ npx vercel deploy --prod
   <h1>Welcome to nateykim.com 👋</h1>
   <p>This is my new personal website built with SvelteKit!</p>
   <p>Nice to meet you, {name}.</p>
+
+  <img src="/mookie.jpeg" alt="Mookie" />
+
 </main>
